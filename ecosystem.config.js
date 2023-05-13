53096618c7c61890da8496b8c762134d649764e4
ashengtan <shengtann@gmail.com>
2023-05-13 16:23:23 +0800

    feat: add dockerfile

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [{
+    name: 'nuxt3-demo',
+    script: './.output/server/index.mjs',
+    exec_mode: 'cluster_mode',
+    min_uptime: '60s',
+    instances: 4,
+    autorestart: true,
+    max_restarts: 30,
+    error_file: '/app/logs/nuxt3-demo/app-err.log',
+    out_file: '/app/logs/nuxt3-demo/app-out.log',
+    merge_logs: true,
+    log_date_format: 'YYYY-MM-DD HH:mm:ss',
+  }],
+}
